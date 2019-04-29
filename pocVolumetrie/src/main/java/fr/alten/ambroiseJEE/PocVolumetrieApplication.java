da47db1312db7ecbcbc4695aa5447f16f0b3f6a7
Andy CHABALIER <andy.chabalier@alten.com>
2019-04-29 10:13:37 +0200

    Project initialisation

---
+++ pocVolumetrie/src/main/java/fr/alten/ambroiseJEE/PocVolumetrieApplication.java
@@ -0,0 +1,13 @@
+package fr.alten.ambroiseJEE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocVolumetrieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocVolumetrieApplication.class, args);
+	}
+
+}
